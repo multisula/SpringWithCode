4689412d317331dd81109f226be53a3bdbc0b659
sulad <sulad@localhost.localdomain>
2023-04-01 17:52:24 +0900

    board crud & test

--- src/main/java/sp1/sp1/mapper/BoardMapper.java
+++ src/main/java/sp1/sp1/mapper/BoardMapper.java
@@ -1,11 +1,35 @@
 package sp1.sp1.mapper;
 
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import sp1.sp1.domain.BoardVO;
 
 import java.util.List;
 
 public interface BoardMapper {
-    @Select("select * from tbl_board where bno > 0")
+    @Select("SELECT * FROM tbl_board WHERE bno > 0")
     public List<BoardVO> getList();
+
+    @Insert("INSERT INTO tbl_board(bno, title, content, writer) " +
+            "VALUES(SEQ_BOARD.NEXTVAL, #{title}, #{content}, #{writer})")
+    public void insert(BoardVO boardVO);
+
+    @SelectKey(statement = "SELECT SEQ_BOARD.NEXTVAL FROM DUAL",
+            keyProperty = "bno", before = true, resultType = Long.class)
+    @Insert("INSERT INTO tbl_board(bno, title, content, writer) " +
+            "VALUES(#{bno}, #{title}, #{content}, #{writer})")
+    public void insertSelectKey(BoardVO boardVO);
+
+    @Select("SELECT * FROM tbl_board WHERE bno = #{bno}")
+    public BoardVO read(Long bno);
+
+    @Delete("DELETE FROM tbl_board WHERE bno = #{bno}")
+    public int delete(Long bno);
+
+    @Update("UPDATE tbl_board SET " +
+            "title = #{title}, " +
+            "content = #{content}, " +
+            "writer = #{writer}, " +
+            "updateDate = SYSDATE " +
+            "WHERE bno = #{bno}")
+    int update(BoardVO boardVO);
 }
--- src/test/java/sp1/sp1/mapper/BoardMapperTest.java
+++ src/test/java/sp1/sp1/mapper/BoardMapperTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import sp1.sp1.config.RootConfig;
+import sp1.sp1.domain.BoardVO;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,5 +23,54 @@ class BoardMapperTest {
     @Test
     public void testGetList(){
         mapper.getList().forEach(board -> log.info(board));
+    }
+
+    @Test
+    public void testInsert(){
+        BoardVO boardVO = new BoardVO();
+        boardVO.setTitle("새로 작성하는 글");
+        boardVO.setContent("새로 작성하는 내용");
+        boardVO.setWriter("newbie");
+
+        mapper.insert(boardVO);
+
+        log.info(boardVO);
+    }
+
+    @Test
+    public void testInsertSelectKey(){
+        BoardVO boardVO = new BoardVO();
+        boardVO.setTitle("새로 작성하는 글 select key");
+        boardVO.setContent("새로 작성하는 내용 select key");
+        boardVO.setWriter("newbie");
+
+        mapper.insertSelectKey(boardVO);
+
+        log.info(boardVO);
+    }
+
+    @Test
+    public void testRead() {
+        BoardVO boardVO = mapper.read(63L);
+
+        log.info(boardVO);
+    }
+
+    @Test
+    public void testDelete() {
+        log.info("DELETE COUNT: " + mapper.delete(63L));
+    }
+
+    @Test
+    public void testUpdate(){
+        BoardVO boardVO = new BoardVO();
+        boardVO.setBno(64L);
+        boardVO.setTitle("수정된 제목");
+        boardVO.setContent("수정된 내용");
+        boardVO.setWriter("user00");
+
+        int count = mapper.update(boardVO);
+
+        log.info("UPDATE COUNT: " + count);
     }
 }
